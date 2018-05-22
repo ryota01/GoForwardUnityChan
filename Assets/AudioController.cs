9734625c299463264226e221cf106907d684a2c1
ryota01 <goatstorm01@gmail.com>
2018-05-22 12:40:12 +0900

    Lesson7の課題
    Lesson7の通常課題です。

---
+++ Assets/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    // 鳴らすSE
+    public AudioClip block;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            if (collision.gameObject.tag == "CubeTag" || collision.gameObject.tag == "GroundTag")
+            {
+                // cubeとgroundに接触したら一回音を鳴らす
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.PlayOneShot(block);
+            }
+        }
+    }
